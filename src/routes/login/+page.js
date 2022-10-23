da08619df69143b75df2f3077a8d706906c71a81
Your Name <you@example.com>
2022-10-23 15:06:03 +0000

    tell api what url state we're using

--- src/routes/login/+page.js
+++ src/routes/login/+page.js
@@ -1,13 +1,13 @@
 import { redirect, error } from '@sveltejs/kit';
  
 /** @type {import('./$types').PageLoad} */
-export async function load({ params, fetch }) {
+export async function load({ params, fetch, url }) {
     let res = await fetch(`https://sovngarde.infinitybots.gg/herpes/auth`)
 
     if (res.ok) {
         let redirectUrl = await res.text();
 
-        throw redirect(302, redirectUrl);
+        throw redirect(302, redirectUrl+`&state=${url.host}`);
     } else {
         let errorData = await res.text();
         throw error(500, errorData);
