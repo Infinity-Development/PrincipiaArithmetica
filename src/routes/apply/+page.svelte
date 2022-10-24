eafeaee7838b2ac230b83ee3fd7a02fa192428a8
bg <bg@gmail.com>
2022-10-24 16:42:49 +0530

    bug fixes + interview
    Co-authored-by: 41LL0N <dillonveit1@gmail.com>

--- src/routes/apply/+page.svelte
+++ src/routes/apply/+page.svelte
@@ -4,20 +4,81 @@
 	import DefaultCard from "$lib/components/DefaultCard.svelte";
 	import Input from "$lib/components/Input.svelte";
 
+    let interview: any[] | null = null
+
     function validateQuestions() {
         let questionMap = new Map();
 
-        $page.data.questions.foreach((question: any) => {
+        console.log($page.data.questions);
+
+        let flag = true;
+
+        $page.data.questions.forEach((question: any) => {
+            if(!flag) {
+                return;
+            }
+
             let userInpValue = (document.querySelector(`#${question.id}`) as HTMLInputElement).value;
 
+            console.log(userInpValue)
+
             if(!userInpValue || userInpValue.length < 50) {
-                return false;
+                flag = false;
+                return
             }
 
             questionMap.set(question.id, userInpValue);
         });
 
-        return questionMap;
+        if(!flag) {
+            return false;
+        }
+
+        flag = true
+
+        if(interview) {
+            let interviewMap = new Map();
+
+            interview.forEach((question: any) => {
+                if(!flag) {
+                    return;
+                }
+
+                let userInpValue = (document.querySelector(`#${question.id}`) as HTMLInputElement).value;
+
+                if(!userInpValue || userInpValue.length < 30) {
+                    flag = false;
+                    return;
+                }
+
+                interviewMap.set(question.id, userInpValue);
+            });
+
+            return [questionMap, interviewMap];
+        }
+
+        return true;
+    }
+
+    async function nextStep() {
+        let validate = validateQuestions()
+
+        console.log(validate)
+
+        if(!validate) {
+            // Dillon, can you pls make this better?
+            alert("Please answer all questions");
+            return;
+        }
+
+        let res = await fetch("https://sovngarde.infinitybots.gg/herpes/zoster")
+
+        if(!res.ok) {
+            alert("Something went wrong, please try again later");
+            return;
+        }
+
+        interview = await res.json();
     }
 </script>
 
@@ -40,4 +101,16 @@
     >{question.para}</Input>
 {/each}
 
-<Button link={"javascript:void(0)"}>Next</Button>
+{#if !interview}
+    <Button onclick={() => nextStep()} link={"javascript:void(0)"}>Next</Button>
+{:else}
+    <h5 class="text-xl font-semibold">Interview</h5>
+    {#each interview as question}
+        <Input
+            id={question.id}
+            label={question.question}
+            placeholder={question.placeholder}
+            minlength={30}
+        >{question.para}</Input>
+    {/each}
+{/if}
