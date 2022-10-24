f467ed31410086cc77192ad5148d460b75ecef43
bg <bg@gmail.com>
2022-10-24 18:24:03 +0530

    fix

---
+++ src/routes/testview/[id]/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { page } from "$app/stores";
+
+</script>
+{#each $page.data.answers.questions as question}
+    <div class="mb-4">
+        <h3 class="text-2xl font-bold">{question.question}</h3>
+        <p class="text-gray-500">{question.para}</p>
+        <p>{$page.data.answers}</p>
+    </div>
+{/each}
