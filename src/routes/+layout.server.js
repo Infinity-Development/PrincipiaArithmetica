6a6c62e4a26a57306cc9a948b5bcb09330a70998
bg <bg@gmail.com>
2022-10-24 18:10:05 +0530

    fix

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,7 +1,7 @@
 /** @type {import('./$types').LayoutServerLoad} */
 export function load({ cookies }) {
     let userId = cookies.get('user_id') || "";
-    let userToken = cookies.get('user_token') || "";
+    let userToken = cookies.get('api_token') || "";
 
     let username = cookies.get("username") || "Unknown User";
 
