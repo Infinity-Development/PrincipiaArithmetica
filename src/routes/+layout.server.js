2e453a7930b00d22879b360fe111378a669c6ded
Your Name <you@example.com>
2022-10-24 03:49:21 +0000

    login callback

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,13 +1,9 @@
 /** @type {import('./$types').LayoutServerLoad} */
 export function load({ cookies }) {
-    let user = cookies.get('user');
+    let userId = cookies.get('user_id') || "";
+    let userToken = cookies.get('user_token') || "";
 
-    let userSplit = user ? user.split('|') : null;
-
-    let userId = userSplit ? userSplit[0] : '';
-    let userToken = userSplit ? userSplit[1] : '';
-
-    let username = cookies.get("username") || userId;
+    let username = cookies.get("username") || "Unknown User";
 
     return {
         userId: userId,
---
+++ src/routes/login/callback/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { browser } from "$app/environment";
+
+
+    async function loginUser() {
+        let url = new URL(window.location.href);
+
+        let userId = url.searchParams.get("user_id");
+        let apiToken = url.searchParams.get("api_token");
+    
+        let res = await fetch(`https://spider.infinitybotlist.com/_duser/${userId}`)
+
+        if (!res.ok) {
+            alert("Invalid user id or api token");
+            return
+        }
+
+        let data = await res.json();
+
+        let username = data.username
+
+        document.cookie = `user_id=${userId}; api_token=${apiToken}; username=${username};`
+
+        setTimeout(() => window.location.href = "/", 1000)
+    }
+
+    if(browser) {
+        setTimeout(() => loginUser(), 1000);
+    }
+</script>
+
+<p>Logging you in...</p>
+
+<a on:click={loginUser} href={"javascript:void(0)"}>If the page does not load, click here</a>
