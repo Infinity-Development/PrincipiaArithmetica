71efa02f33565b7893f1288274dcf76047c785bb
bg <bg@gmail.com>
2022-10-24 18:34:35 +0530

    add send interview button

--- src/routes/testview/[id]/+page.svelte
+++ src/routes/testview/[id]/+page.svelte
@@ -1,7 +1,10 @@
 <script>
 	import { page } from "$app/stores";
+	import Button from "$lib/components/Button.svelte";
 
 </script>
+<h2 class="text-3xl font-bold">App State: {$page.data.position}</h2>
+
 {#each $page.data.answers.questions as question}
     <div class="mb-4">
         <h3 class="text-2xl font-bold">{question.question}</h3>
@@ -9,3 +12,7 @@
         <p>{$page.data.answers.answers[question.id]}</p>
     </div>
 {/each}
+
+{#if $page.data.position == "pending"}
+    <Button link={"javascript:void(0)"}>Send Interview</Button>
+{/if}
