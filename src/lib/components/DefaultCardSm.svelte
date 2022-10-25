c7bd383d28b26e8b6ef09b4a3db41ddb2a308ce8
bg <bg@gmail.com>
2022-10-25 16:37:59 +0530

    change a to article

--- src/lib/components/DefaultCardSm.svelte
+++ src/lib/components/DefaultCardSm.svelte
@@ -7,7 +7,7 @@
 </script>
 
 
-<a href={"javascript:void(0)"} on:click={onclick} class="block p-6 m-3 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+<article on:keypress={onclick} on:click={onclick} class="block p-6 m-3 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
     <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{@html title}</h5>
     <p class="font-normal text-gray-700 dark:text-gray-400"><slot /></p>
-</a>
+</article>
