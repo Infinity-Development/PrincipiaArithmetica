c62e1b44bf07a27271421fa94c6e34a6c26d00f3
bg <bg@gmail.com>
2022-10-25 09:10:26 +0530

    add interview api

--- src/routes/testview/[id]/+page.svelte
+++ src/routes/testview/[id]/+page.svelte
@@ -2,6 +2,22 @@
 	import { page } from "$app/stores";
 	import Button from "$lib/components/Button.svelte";
 
+
+    function sendInterview() {
+        fetch(`https://sovngarde.infinitybots.gg//herpes/app/interview:?user_id=${$page.data.userId}&app_id=${$page.params.id}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": $page.data.userToken
+            },
+        }).then((res) => {
+            if(res.ok) {
+                alert("Interview sent!");
+            } else {
+                alert("Something went wrong!");
+            }
+        });
+    }
 </script>
 <h2 class="text-3xl font-bold">App Info</h2>
 <p>App State: {$page.data.state}</p>
@@ -17,5 +33,5 @@
 {/each}
 
 {#if $page.data.state == "pending"}
-    <Button link={"javascript:void(0)"}>Send Interview</Button>
+    <Button link={"javascript:void(0)"} onclick={sendInterview}>Send Interview</Button>
 {/if}
