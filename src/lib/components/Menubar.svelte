59fcab0ce5750fe924ef05e5b9ae7616bae43de1
bg <bg@gmail.com>
2022-10-26 11:52:07 +0530

    ...

--- src/lib/components/Menubar.svelte
+++ src/lib/components/Menubar.svelte
@@ -31,7 +31,7 @@
 <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
   <div class="flex flex-wrap justify-between items-center mx-auto">
     <a href="/" class="flex items-center text-black">
-        <img src="https://cdn.infinitybots.xyz/images/png/Infinity.png" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
+        <img src="https://cdn.infinitybots.xyz/images/png/Infinity.png" class="mr-3 h-6 sm:h-9" alt="IBL Logo" />
         <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Infinity Portal</span>
     </a>
     <button on:click={onClickMenu} type="button" class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
