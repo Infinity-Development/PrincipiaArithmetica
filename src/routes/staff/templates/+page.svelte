812e8e6520608e325aaa941fe02b4d741a9cf16f
bg <bg@gmail.com>
2022-10-26 09:23:37 +0530

    fix some css a bit and add more templates

--- src/routes/staff/templates/+page.svelte
+++ src/routes/staff/templates/+page.svelte
@@ -6,6 +6,7 @@ import Template from "./Template.svelte";
 
     let listName = "[list name]"
     let commands = "[commands/features here]"
+    let linkToBot = "[Link to Bot](https://somelink.com)"
 </script>
 
 <h5 class="text-2xl font-semibold">Variables</h5>
@@ -38,6 +39,15 @@ import Template from "./Template.svelte";
 
 <div class="mt-5"></div>
 
+<InputSm
+    id="linkToBot"
+    label="Link To Bot"
+    placeholder="Link To Bot"
+    minlength={0}
+    bind:value={linkToBot}
+>Enter the "link to bot" here if you want this page to reflect specific bots that have been copied or are otherwise identical to.</InputSm>
+
+<div class="mt-5"></div>
 
 <!--This is where the actual content begins-->
 <h5 class="text-2xl font-semibold">Approval Templates</h5>
@@ -158,4 +168,121 @@ Your bot doesnt have a (working) help command or obvious point of entry.
 Please make sure your bot has a help command or has an explanation in the bot description.
 
 Note that if you are using slash commands, then you do not need a help command" />
+    <Template
+        title="Suicide/Gore Promotion"
+        icon="🔪"
+        reason="
+Your bot has a suicide command which is considered as glorification/promotion of suicide, which is against Discord ToS. 
+
+Please remove this command entirely." />
+    <Template
+        title="BDFD Fork or Template"
+        icon="🤖"
+        reason='
+Your bot seems to be an unmodified instance of [Bot Designer for Discord](https://botdesignerdiscord.com/).
+
+We dont allow unmodified clones of other bots or bot creation services.
+        ' />
+    <Template
+        title="Cross Promotion"
+        icon="👀"
+        reason="
+Your bots page is filled out with a link/button to another bot listing website. 
+
+Please remove any references to other bot listing websites in your description and re-apply." />
+    <Template
+        title="Seizure Inducing Content"
+        icon="🏨"
+        reason="
+Your bots commands have emojis or gifs that could cause epileptic seizures due to its flashy and flickering nature. 
+
+Please remove all content of such nature in your commands.
+        " />
+    <Template
+        title="NSFW Features/Content"
+        icon="🔞"
+        reason="
+Your bot page mentions, or promotes NSFW functions in its descriptions. 
+
+Please remove any mentioning of NSFW features and make sure that all the NSFW functions are locked for [NSFW channels](https://support.discord.com/hc/en-us/articles/115000084051-NSFW-Channels-and-Content)." />
+    <Template
+        title="Unmodified Instance"
+        icon="🤖"
+        reason="
+Your bot seems to be an unmodified instance of: {linkToBot}. 
+
+We dont allow unmodified clones of other bots. Please note for BotGhost/BDFD clones in particular, we require 5 or more custom commands." />
+    <Template
+        title="J4J/Third Party Ads"
+        icon="🔖"
+        reason="
+Your bot is promoting sponsors/partners/servers. 
+
+Bots are not allowed to use the Discord API to advertise/promote any third-party service. 
+
+As stated in their [Terms](https://i.imgur.com/eTLRu3m.png), you may not use the APIs in any way to target users with advertisements or marketing. 
+
+If your bot has a Join4Join feature, please read Discord's stance regarding bots of this nature here or join the [Discord Developers](https://discord.gg/discord-developers) server for more information." />
+    <Template
+        title="Flagged/Banned Bot"
+        icon="⚠️"
+        reason="
+Your bot has been flagged by Discord for one of the following reasons (Spam, Abusive Behaviour, reaching guild limit without verification, verified successfully by Discord but using privileged intents that your application was not whitelisted for.) which results in us being unable to test your bot. 
+
+For more information, please reach out Discord directly [here](https://dis.gd/contact) if you have questions. 
+
+Please resolve this issue before reapplying." />
+    <Template
+        title="No Functions/Features"
+        icon="🤷‍♂️"
+        reason="
+Your bot doesnt have any actual (functioning) features/commands. 
+
+We require bots to have a minimum of at least 7 working commands not including the help and about commands. 
+
+Please add some features and or commands to your bot before re-applying!" />
+    <Template
+        title="Main Owner Banned"
+        icon="⚖️"
+        reason="
+The primary owner of this bot was banned from our discord server. As such they are prohibited from adding any bots. 
+
+They can however appeal any and all bans by contacting our Support Team" />
+    <Template
+        title="Admin Bots"
+        icon="⚔️"
+        reason="
+Your bot is asking for the admin permission on invite. No bot should require this kind of permission to function correctly.
+
+Please properly setup command permissions on your bots commands to what they *actually need* and reapply." />
+    <Template
+        title="Unknown Application"
+        icon="🤔"
+        reason="
+There is an unknown application error when trying to invite your bot. 
+
+Please make sure that the application ID you entered is correct, you have a bot user with your application and your bot application wasnt deleted.
+
+You must fix this issue before reapplying." />
+    <Template
+        title="Broken Commands"
+        icon="💻"
+        reason="
+The majority of your commands listed on your bots page, or help command do not provide a response, or do not seem to function/work. 
+
+Please resolve this issue and resubmit!" />
+    <Template
+        title="Public Owner/Dev Commands"
+        icon="🔓"
+        reason="
+Your bot has an owner only command(s) that allows users to access potential vulnerabilities or features that should be locked to developers. 
+
+Please lock these commands for developers/owners only and re-apply." />
+    <Template
+        title="Running Multiple Instances"
+        icon="👨‍👩‍👧‍👦"
+        reason="
+Your bot application seems to be running multiple instances, which could cause unhandled ratelimits and api abuse, as well as spam. 
+
+Please be sure that your bot isnt running on multiple instances prior to resubmitting." />
 </div>
