0e88f5356737755097e7e166e61bc0b9825b4092
bg <bg@gmail.com>
2022-10-26 11:51:46 +0530

    make menucard not awkward

--- src/lib/components/Menubar.svelte
+++ src/lib/components/Menubar.svelte
@@ -29,7 +29,7 @@
 </script>
 
 <nav class="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
-  <div class="container flex flex-wrap justify-between items-center mx-auto">
+  <div class="flex flex-wrap justify-between items-center mx-auto">
     <a href="/" class="flex items-center text-black">
         <img src="https://cdn.infinitybots.xyz/images/png/Infinity.png" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
         <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Infinity Portal</span>
