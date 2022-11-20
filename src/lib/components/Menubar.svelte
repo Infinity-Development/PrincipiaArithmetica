360392237d29d4ce7917fd4bd6e0bc1496fb3713
Your Name <you@example.com>
2022-11-20 18:38:13 +0530

    ...

--- src/lib/components/Menubar.svelte
+++ src/lib/components/Menubar.svelte
@@ -49,9 +49,12 @@
         <li>
           <a href={"/blog"} class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Blog</a>
         </li>
+        <li>
+          <a href={"/data"} class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Data Actions</a>
+        </li>
         {#if $page.data.userId}
           <li>
-            <a href={"javascript:void(0)"} on:click={logoutUser} class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Logout (signed in as {$page.data.username})</a>
+            <a href={"javascript:void(0)"} on:click={logoutUser} class="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Logout ({$page.data.username})</a>
           </li>
         {:else}
           <li>
