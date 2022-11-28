39e08ef8824c6359cddaf31e0e156380b807ba7a
Your Name <you@example.com>
2022-11-28 11:17:03 +0530

    add staff checks

--- src/routes/panel/+layout.server.js
+++ src/routes/panel/+layout.server.js
@@ -1,14 +1,37 @@
 /** @type {import('./$types').LayoutServerLoad} */
-export function load({ cookies }) {
-    let userId = cookies.get('user_id') || "";
-    let userToken = cookies.get('api_token') || "";
+export async function load({ parent, fetch }) {
+    let session = await parent();
 
-    let username = cookies.get("username") || "Unknown User";
+    if(!session || !session.userId) {
+        return {
+            staff: false,
+            admin: false,
+            hadmin: false,
+            loggedIn: false,
+            user: null,
+        }
+    }
 
-    return {
-        userId: userId,
-        userToken: userToken,
-        username: username,
+    // Query /users/:id
+    let res = await fetch(`https://spider.infinitybots.gg/users/${session.userId}`);
+
+    if (res.ok) {
+        let resp = await res.json();
+        return {
+            staff: resp.staff || false,
+            admin: resp.admin || false,
+            hadmin: resp.hadmin || false,
+            loggedIn: true,
+            user: resp || null,
+        }
+    } else {
+        return {
+            staff: false,
+            admin: false,
+            hadmin: false,
+            loggedIn: true,
+            user: null,
+        }
     }
 }
   
--- src/routes/panel/components/PanelSidebar.svelte
+++ src/routes/panel/components/PanelSidebar.svelte
@@ -1,4 +1,9 @@
 <script>
+    import { page } from "$app/stores"
+	import GreyText from "$lib/components/GreyText.svelte";
+
+    console.log($page.data)
+
     let svgClassList = "w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white";
 </script>
 
@@ -6,9 +11,21 @@
     <aside class="hidden md:block sticky top-0 stick-ontop" style="display: unset">
         <ul class="space-y-2">
             <li>
-            <span class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white">
-                <h1 class="font-semibold text-xl">Quick Navigation</h1>
-            </span>
+                <span class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white">
+                    <div>
+                        <h1 class="font-semibold text-xl">Quick Navigation</h1>
+                        <div class="mt-1"></div>
+                        <small>
+                            <strong>
+                                {#if !$page.data.loggedIn}
+                                    You are not logged in.
+                                    <div></div>
+                                {/if}
+                                Heres what you can do!
+                            </strong>
+                        </small>
+                    </div>
+                </span>
             </li>
             <li>
                 <a href="/panel" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
@@ -16,30 +33,36 @@
                 <span class="ml-3">Statistics</span>
                 </a>
             </li>
-            <li>
-                <a href="/panel/queue" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                <svg aria-hidden="true" class={svgClassList} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path></svg>
-                <span class="flex-1 ml-3 whitespace-nowrap">Queue</span>
-                </a>
-            </li>
-            <li>
-                <a href="/panel/certify" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                <svg aria-hidden="true" class={svgClassList} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z"></path><path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z"></path></svg>
-                <span class="flex-1 ml-3 whitespace-nowrap">Certification</span>
-                </a>
-            </li>
-            <li>
-                <a href="/panel/users" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                <svg aria-hidden="true" class={svgClassList} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
-                <span class="flex-1 ml-3 whitespace-nowrap">Users</span>
-                </a>
-            </li>
-            <li>
-                <a href="/panel/bots" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                <svg aria-hidden="true" class={svgClassList} xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="currentColor" d="M21 10.975V8a2 2 0 0 0-2-2h-6V4.688c.305-.274.5-.668.5-1.11a1.5 1.5 0 0 0-3 0c0 .442.195.836.5 1.11V6H5a2 2 0 0 0-2 2v2.998l-.072.005A.999.999 0 0 0 2 12v2a1 1 0 0 0 1 1v5a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-5a1 1 0 0 0 1-1v-1.938a1.004 1.004 0 0 0-.072-.455c-.202-.488-.635-.605-.928-.632zM7 12c0-1.104.672-2 1.5-2s1.5.896 1.5 2s-.672 2-1.5 2S7 13.104 7 12zm8.998 6c-1.001-.003-7.997 0-7.998 0v-2s7.001-.002 8.002 0l-.004 2zm-.498-4c-.828 0-1.5-.896-1.5-2s.672-2 1.5-2s1.5.896 1.5 2s-.672 2-1.5 2z"/></svg>
-                <span class="flex-1 ml-3 whitespace-nowrap">Bots</span>
-                </a>
-            </li>
+            {#if $page.data.staff}
+                <li>
+                    <a href="/panel/queue" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                    <svg aria-hidden="true" class={svgClassList} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path></svg>
+                    <span class="flex-1 ml-3 whitespace-nowrap">Queue</span>
+                    </a>
+                </li>
+            {/if}
+            {#if $page.data.admin}
+                <li>
+                    <a href="/panel/certify" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                    <svg aria-hidden="true" class={svgClassList} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z"></path><path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z"></path></svg>
+                    <span class="flex-1 ml-3 whitespace-nowrap">Certification</span>
+                    </a>
+                </li>
+            {/if}
+            {#if $page.data.staff}
+                <li>
+                    <a href="/panel/users" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                    <svg aria-hidden="true" class={svgClassList} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
+                    <span class="flex-1 ml-3 whitespace-nowrap">Users</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="/panel/bots" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                    <svg aria-hidden="true" class={svgClassList} xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><path fill="currentColor" d="M21 10.975V8a2 2 0 0 0-2-2h-6V4.688c.305-.274.5-.668.5-1.11a1.5 1.5 0 0 0-3 0c0 .442.195.836.5 1.11V6H5a2 2 0 0 0-2 2v2.998l-.072.005A.999.999 0 0 0 2 12v2a1 1 0 0 0 1 1v5a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-5a1 1 0 0 0 1-1v-1.938a1.004 1.004 0 0 0-.072-.455c-.202-.488-.635-.605-.928-.632zM7 12c0-1.104.672-2 1.5-2s1.5.896 1.5 2s-.672 2-1.5 2S7 13.104 7 12zm8.998 6c-1.001-.003-7.997 0-7.998 0v-2s7.001-.002 8.002 0l-.004 2zm-.498-4c-.828 0-1.5-.896-1.5-2s.672-2 1.5-2s1.5.896 1.5 2s-.672 2-1.5 2z"/></svg>
+                    <span class="flex-1 ml-3 whitespace-nowrap">Bots</span>
+                    </a>
+                </li>
+            {/if}
             <li>
                 <a href="/staff/guide" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
                 <svg aria-hidden="true" class={svgClassList} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z" clip-rule="evenodd"></path></svg>
@@ -51,6 +74,19 @@
                 <svg aria-hidden="true" class={svgClassList} fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z" clip-rule="evenodd"></path></svg>
                 <span class="flex-1 ml-3 whitespace-nowrap">Help</span>
                 </a>
+            </li>
+            <li>
+                <span class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white">
+                    <div>
+                        <small>
+                            Staff: {$page.data.staff}
+                            <div></div>
+                            Admin: {$page.data.admin}
+                            <div></div>
+                            HAdmin: {$page.data.hadmin}
+                        </small>
+                    </div>
+                </span>
             </li>
         </ul>
     </aside>
