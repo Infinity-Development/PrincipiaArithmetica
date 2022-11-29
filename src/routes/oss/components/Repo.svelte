ed9c80618f70941a0705462199a9924f5c611bfe
Your Name <you@example.com>
2022-11-29 09:08:27 +0530

    add oss page

---
+++ src/routes/oss/components/Repo.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import Button from "$lib/components/Button.svelte";
+	import GreyText from "$lib/components/GreyText.svelte";
+
+    export let name: string;
+    export let repoName: string;
+    export let description: string;
+</script>
+
+<h3 class="text-xl font-semibold">{name}</h3>
+<GreyText>{description}</GreyText>
+<Button link={`https://github.com/InfinityBotList/${repoName}`}>View {name}}</Button>
