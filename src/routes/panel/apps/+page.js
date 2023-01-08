afcb1a3b0883a5bdedf421826d4361ed04cb0f3f
Your Name <you@example.com>
2023-01-08 18:10:57 +0530

    fix

--- src/routes/panel/apps/+page.js
+++ src/routes/panel/apps/+page.js
@@ -8,20 +8,4 @@ export async function load({ fetch, parent }) {
     if(!session.admin) {
         throw error(403, "You are not a admin.");
     }
-
-    let res = await fetch(`${apiUrl}/users/${session.userId}/apps?full=true`, {
-        headers: {
-            "Authorization": session.userToken
-        }
-    })
-
-    if (res.ok) {
-        let json = await res.json();
-
-        return json;
-    }
-
-    let errorData = await res.json();
-
-    throw error(res.status, errorData.message);
 }
