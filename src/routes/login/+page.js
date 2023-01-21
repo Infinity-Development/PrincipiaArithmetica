49a77f52c20deaa6c212de9b1f0642fa157e87d8
Your Name <you@example.com>
2023-01-21 11:33:51 +0530

    more removals, will update cf at earliest

--- src/routes/login/+page.js
+++
@@ -1,7 +0,0 @@
-import { mainSiteUrl } from '$lib/url';
-import { redirect, error } from '@sveltejs/kit';
- 
-/** @type {import('./$types').PageLoad} */
-export async function load() {
-    throw redirect(302, mainSiteUrl+`/sauron?state=portalLogin`);
-}
